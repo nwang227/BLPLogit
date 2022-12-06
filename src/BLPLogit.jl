45d22249080b91e64b264fed691221322b4b2a5d
paradoxuu <hwang828@student.ubc.ca>
2022-12-05 17:17:41 -0800

    Add a fucntion simulating choices and a BLP parameter struct

--- src/BLPLogit.jl
+++ src/BLPLogit.jl
@@ -1,5 +1,15 @@
 module BLPLogit
-
+struct BLPparameters
+    ρ::Float64
+    T::Int64
+    J::Int64
+    I::Int64
+    σ1::Float64
+    σ2::Float64
+    α::Float64
+    β1::Float64
+    β2::Float64
+end
 # Write your package code here.
 
 end
