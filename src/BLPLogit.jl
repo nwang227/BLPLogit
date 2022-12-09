00beb2a10553fa02e0777e1405cf39aa67233f60
nanUBC <97639296+nanUBC@users.noreply.github.com>
2022-12-08 16:08:34 -0800

    More Character

--- src/BLPLogit.jl
+++ src/BLPLogit.jl
@@ -5,14 +5,16 @@ using CSV, DataFrames, Optim, Random, LinearAlgebra
 export cshare, cdelta, delta_full, obj
 
 #Function for conjectured share for one market
-function cshare(delta::Vector, p::Vector, char1::Vector, v1, v2, σ)
-    ns = length(v1)
+function cshare(delta::Vector, p::Vector, char::Matrix, v::Matrix, σ)
+    ns,k1 = length(v1)
     k = length(p)
     share = zeros(k)
     σ1 = σ[1]
-    σ2 = σ[2]
+    σ2 = σ[2:end]
+    v1 = v[:,1]
+    v2 = v[:,2:end]
     @threads for g in 1:ns
-        num = exp.(delta - σ1 * v1[g] * p + σ2 * v2[g] * char1)
+        num = exp.(delta - σ1 * v1[g] * p + char * (σ2 .* v2[g,:]) )
         den = 1 + sum(num)
         prob = num./den
         share += 1/ns * prob
@@ -22,7 +24,7 @@ end
 
 
 #Function for estimating delta for one market
-function cdelta(share::Vector,p::Vector, char1::Vector, v1, v2, σ,tol)
+function cdelta(p,char,share,v, σ,tol)
     k = length(p)
     delta0 = ones(k)
     delta1 = ones(k)
@@ -30,7 +32,7 @@ function cdelta(share::Vector,p::Vector, char1::Vector, v1, v2, σ,tol)
 
     while error > tol
         delta0 = delta1
-        j = cshare(delta0, p, char1, v1, v2, σ)
+        j = cshare(delta0, p, char, v, σ)
         delta1 = delta0 + log.(share) - log.(j)
         error = (delta1 - delta0)' * (delta1 - delta0)
     end
@@ -40,7 +42,7 @@ end
 
 
 #Estimate delta for all the markets
-function delta_full(σ, df, v1, v2)
+function delta_full(σ, p,char,share,v)
     m = maximum(df[:,:market_id])
     δ = zeros(3*m)
 
@@ -48,15 +50,15 @@ function delta_full(σ, df, v1, v2)
         p = df[df.market_id .== j , :p]
         char1 = df[df.market_id .== j , :char1]
         share = df[df.market_id .== j , :share]
-        δ_new = cdelta(share, p , char1, v1, v2, σ,tol)
+        δ_new = cdelta(p,char,share,v, σ,tol)
         δ[(j-1)*3 + 1:(j-1)*3 + 3] =  δ_new
     end
     return δ
 end
 
 #Objective function, spit out both GMM error and β
-function obj(σ ,df, v1, v2)
-    δ = delta_full(σ, df, v1, v2)
+function obj(σ ,p,char,share,v)
+    δ = delta_full(σ, p,char,share,v)
     p = df[:, :p]
     char1 = df[: , :char1]
     char2 = df[: , :char2]
@@ -70,6 +72,12 @@ function obj(σ ,df, v1, v2)
     res = u' * z * w * z' * u
     return (res = res, beta = beta[2:4])
 end
+
+
+function est_mixed(p, char, share, z, tol, NS)
+    n,k = size(char)
+    v = randn(NS, k+1)
+    
 
 
 end
