5a75acb0c1dc98f3e518ae3f65198e26b5fa9bcf
paradoxuu <hwang828@student.ubc.ca>
2022-12-13 15:56:40 -0800

    Complete the generalized simulations

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,48 +6,49 @@ using LinearAlgebra, Distributions, Random,UnPack
 end
 
 struct BLPparameters
-    ρ::Float64
     T::Int64
     J::Int64
     I::Int64
-    σ1::Float64
-    σ2::Float64
+    σ::AbstractVector
     α::Float64
-    β1::Float64
-    β2::Float64
+    β::AbstractVector
 end
+
+pa= BLPparameters(10,10,10,[-1,2,1],1,[1,1,1])
 
 
 
 function simlate_utility(pa::BLPparameters)
-    @unpack ρ,T,J,I,σ1,σ2,α,β1,β2 = pa
-    ϵ= reshape(rand(Logistic(),J*I),J,I)
-    char1 = reshape(rand(Normal(0,1),J*I),J,I)
-    char2 = reshape(rand(Normal(0,1),J*I),J,I)
-    p = reshape(rand(Normal(0,1),J*I),J,I)
-    ξ = reshape(rand(Normal(0,1),J*I),J,I)
-    δ = -α .*p + β1 .* char1 + β2 .* char2 + ξ
-    ν1 = rand(Normal(0, 1),I)
-    ν2 = rand(Normal(0, 1),I)
-    
+    @unpack T,J,I,σ,α,β = pa
+    ϵ= rand(Logistic(),J*T*I)
+    mvnormalchar= MvNormal(zeros(length(β)), Array(Diagonal(ones(length(β)))));
+    char = rand(mvnormalchar,J*T)'
+    p = rand(Normal(0,1),J*T)
+    ξ = rand(Normal(0,1),J*T)
+    δ = reshape(-α .*p + char*β + ξ, J,T)
+    mvnormalv= MvNormal(zeros(length(σ)), Array(Diagonal(ones(length(σ)))));
+    v = rand(mvnormalv,I)'
     U = zeros(T,J,I)
 
+    pchar = hcat(p, char[:,1:length(σ)-1])
+    ϵm =  reshape(ϵ,I,J,T)
     for i in 1:T
         for j in 1:J
             for t in 1:I
-                 U[i,j,t]= δ[j,t] - σ1*ν1[i]*p[j,t] + σ2*ν2[i]*char1[j,t] + ϵ[j,t]
+                 U[i,j,t]= δ[j,t] + sum( σ .* v[i,:]' .* pchar[(j-1)*T + t, :]) + ϵm[i,j,t]
             end
         end
     end
 
-    return (p, char1, char2, U) = (p, char1, char2, U)
+
+    return (p, char, U) = (p, char, U)
 end
 
-simlate_utility(pa::BLPparameters)
+A = simlate_utility(pa::BLPparameters)
+A[3]
 
 function simulate_BLP(pa::BLPparameters)
-    A = simlate_utility(pa::BLPparameters)
-    U = A[4]
+    U = simlate_utility(pa::BLPparameters)[3]
     @unpack T,I = pa
     c = zeros(I,T)
     for i in 1:I
@@ -55,11 +56,11 @@ function simulate_BLP(pa::BLPparameters)
         c[i,t] = findmax(U[i,:,t])[2]
         end 
     end 
-    return (p= A[1],char1=A[2],char2=A[3] ,c=c)
+    return (p= A[1],char=A[2] ,c=c)
 end
 
-pa = BLPparameters(3,10,10,10,1,1,1,1,1)
-simulate_BLP(pa)
+simulate_BLP(pa)[3]
+
 
 
 
