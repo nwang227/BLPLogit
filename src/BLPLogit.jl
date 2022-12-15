4100837de3c9bb39f813d62ee549d0b00020b2b1
nanUBC <97639296+nanUBC@users.noreply.github.com>
2022-12-14 17:56:00 -0800

    full function

--- src/BLPLogit.jl
+++ src/BLPLogit.jl
@@ -4,6 +4,21 @@ module BLPLogit
 using CSV, DataFrames, Optim, Random, LinearAlgebra
 export cshare, cdelta, delta_full, obj
 
+
+
+#Function for constructing desired data set
+
+function blpdata(df::DataFrame, p::Symbol, s::Symbol, char::Vector{Symbol},id::Symbol, cost::Vector{Symbol})
+    price = select(df,[p,id])
+    share = select(df, [s,id])
+    character = select(df, char)
+    c = select(df, c)
+    id = select(df,id)
+    ch = hcat(character, id)
+    return (price = price, share = share, ch = ch, c = c)
+end
+
+
 #Function for conjectured share for one market
 function cshare(delta::Vector, p::Vector, char::Matrix, v::Matrix, σ)
     ns,k1 = length(v1)
@@ -13,7 +28,7 @@ function cshare(delta::Vector, p::Vector, char::Matrix, v::Matrix, σ)
     σ2 = σ[2:end]
     v1 = v[:,1]
     v2 = v[:,2:end]
-    @threads for g in 1:ns
+    for g in 1:ns
         num = exp.(delta - σ1 * v1[g] * p + char * (σ2 .* v2[g,:]) )
         den = 1 + sum(num)
         prob = num./den
@@ -42,30 +57,31 @@ end
 
 
 #Estimate delta for all the markets
-function delta_full(σ, p,char,share,v)
-    m = maximum(df[:,:market_id])
-    δ = zeros(3*m)
+function delta_full(p,ch,s,v, tol)
+    m = maximum(p[:,2])
+    n,k = size(ch)
+    δ = [ ]
 
-    @threads for j in 1:m
-        p = df[df.market_id .== j , :p]
-        char1 = df[df.market_id .== j , :char1]
-        share = df[df.market_id .== j , :share]
+    for j in 1:m
+        p = p[p[:,2] == j, 1] 
+        char = ch[ch[:,k] == j, 1:k-1]
+        share = s[s[:,2] == j, 1]
         δ_new = cdelta(p,char,share,v, σ,tol)
-        δ[(j-1)*3 + 1:(j-1)*3 + 3] =  δ_new
+        vcat(δ, δ_new)
     end
     return δ
 end
 
 #Objective function, spit out both GMM error and β
-function obj(σ ,p,char,share,v)
-    δ = delta_full(σ, p,char,share,v)
-    p = df[:, :p]
-    char1 = df[: , :char1]
-    char2 = df[: , :char2]
-    share = df[: , :share]
-    z1 = Matrix(df[: , [:cost1, :cost2, :cost3]])
-    x = [ones(1800) p  char1  char2]
-    z = [ones(1800) char1 char2 z1]
+function obj(σ ,p,ch,s,v,c,tol)
+    n,k = size(ch)
+    δ = delta_full(p,ch,s,v,tol)
+    p = p[:,1]
+    char = Matrix(ch[:, 1:k-1])
+    share = s[: , 1]
+    z1 = Matrix(c)
+    x = hcat(ones(1800), p, char)
+    z = hcat(ones(1800), char, z1)
     w = inv(z' * z)
     beta = (x'* z * w * z' * x)^-1  * (x'* z * w * z' * δ)
     u = δ - x * beta
@@ -74,10 +90,13 @@ function obj(σ ,p,char,share,v)
 end
 
 
-function est_mixed(p, char, share, z, tol, NS)
+function est_mixed(p, s,char, c, NS, ini, tol)
     n,k = size(char)
     v = randn(NS, k+1)
-    
 
+    result1 = optimize(sig -> obj(sig, p, ch,s,c,v,tol)[1], ini)
 
+    θ2 = Optim.minimizer(result1)
+    println("θ1 =", round.(obj(θ2 ,p, ch,s,c,v, tol)[2], digits = 3))
+    println("θ2 =",  round.(θ2, digits = 3))
 end
