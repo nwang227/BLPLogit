51d3535e7a6003e8e737d6b521261e02f7ab680d
paradoxuu <hwang828@student.ubc.ca>
2022-12-14 19:30:09 -0800

    Add a fucntion creating data matrix

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,12 +9,13 @@ struct BLPparameters
     T::Int64
     J::Int64
     I::Int64
+    M::Int64
     σ::AbstractVector
     α::Float64
     β::AbstractVector
 end
 
-pa= BLPparameters(10,10,10,[-1,2,1],1,[1,1,1])
+pa= BLPparameters(10,10,10,5,[-1,2,1],1,[1,1,1])
 
 
 
@@ -30,11 +31,12 @@ function simlate_utility(pa::BLPparameters)
     v = rand(mvnormalv,I)'
     U = zeros(T,J,I)
 
+
     pchar = hcat(p, char[:,1:length(σ)-1])
     ϵm =  reshape(ϵ,I,J,T)
-    for i in 1:T
+    for i in 1:I
         for j in 1:J
-            for t in 1:I
+            for t in 1:T
                  U[i,j,t]= δ[j,t] + sum( σ .* v[i,:]' .* pchar[(j-1)*T + t, :]) + ϵm[i,j,t]
             end
         end
@@ -47,13 +49,17 @@ end
 A = simlate_utility(pa::BLPparameters)
 A[3]
 
+
 function simulate_BLP(pa::BLPparameters)
+    @unpack T,I,J,M = pa
+    c = zeros(I,T,M)
     U = simlate_utility(pa::BLPparameters)[3]
-    @unpack T,I = pa
-    c = zeros(I,T)
-    for i in 1:I
-        for t in 1:T
-        c[i,t] = findmax(U[i,:,t])[2]
+    for m in 1:M 
+        n = rand(DiscreteUniform(2,J))
+        for i in 1:I
+            for t in 1:T
+            c[i,t,m] = findmax(rand(U[i,:,t],n))[2]
+            end 
         end 
     end 
     return (p= A[1],char=A[2] ,c=c)
@@ -61,6 +67,60 @@ end
 
 simulate_BLP(pa)[3]
 
+@unpack p, char, c = simulate_BLP(pa)
+@unpack T,J,I,M,σ,α,β = pa
+p
+
+function createrep(J,T)
+    v = ones(J)
+    for i in 1: T-1
+    v = vcat(v,ones(J)*(i+1)) 
+    end 
+    return v
+end 
+
+
+function createdataframe(J,T,M)
+    A = repeat([1:J;],T)
+    B = createrep(J,T)
+    C = hcat(p,char,A,B)
+    D = repeat(C,M)
+    E = createrep(J*T,M)
+    s =zeros(J,T,M)
+        for m in 1:M
+            for j in 1:J
+                for t in 1:T
+                    s[j,t,m]= sum(c[:,t, m].==j)/I
+                end 
+            end
+        end 
+
+    a = zeros(1)
+        for m in 1:M 
+        a = vcat(a, reshape(s[:,:,m],J*T,1))
+        end 
+    
+    return hcat(a[2:end],D,E)
+end
+
+DF = createdataframe(J,T,M)
 
 
 
+
+
+
+s =zeros(J,T,M)
+for m in 1:M
+    for j in 1:J
+        for t in 1:T
+            s[j,t,m]= sum(c[:,t, m].==j)/I
+        end 
+    end
+end 
+
+a = zeros(1)
+for m in 1:M 
+a = vcat(a, reshape(s[:,:,m],J*T,1))
+end 
+a[2:end]
